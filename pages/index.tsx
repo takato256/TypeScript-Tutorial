d79541122938981d07872253df6935547ed52f13
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2024-11-05 00:46:50 +0900

    feat 見栄えを整える

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetServerSideProps, NextPage } from "next";
 import { useState } from "react";
+import styles from "./index.module.css";
 
 // getServerSidePropsから渡されるpropsの型
 type Props = {
@@ -11,14 +12,6 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
     const [imageUrl, setImageUrl] = useState(initialImageUrl);  // 初期値を渡す
     const [loading, setLoading] = useState(false);  // 初期状態はfalseにしておく
 
-    // マウント時に画像を読み込む宣言
-    // useEffect(() => {
-    //     fetchImage().then((newImage) => {
-    //         setImageUrl(newImage.url); // 画像URLの状態を更新する
-    //         setLoading(false); // ローディング状態を更新する
-    //     });
-    // }, []);
-
     // ボタンをクリックした時に画像を読み込む処理
     const handleClick = async () => {
         setLoading(true);   // 読み込み中フラグを立てる
@@ -29,9 +22,13 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
 
     // ボタンを押した時に画像を表示する
     return (
-        <div>
-            <button onClick={handleClick}>他のにゃんこも見る</button>
-            <div>{loading || <img src={imageUrl} />}</div>
+        <div className={styles.page}>
+            <button onClick={handleClick} className={styles.button}>
+                他のにゃんこも見る
+            </button>
+            <div className={styles.frame}>
+                {loading || <img src={imageUrl} className={styles.img} />}
+            </div>
         </div>
     );
 };
